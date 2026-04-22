4a07940818fc728036b0a3abb8e82bf41eb3b306
Visionstudio <76199245+VisionStudi0@users.noreply.github.com>
2026-04-22 12:01:38 -0500

    Refactor firebase-config.js by removing comments
    Removed comments and cleaned up the code structure.

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,7 @@
-// Importamos las librerías desde la red (CDN) para que funcionen en GitHub Pages
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
 
-// Tu configuración de Firebase (la que acabas de obtener)
 const firebaseConfig = {
   apiKey: "AIzaSyCTAiCQ7hi-ZmBt2MYWzciELL-lB_SsE3A",
   authDomain: "iku-menu-interactivo.firebaseapp.com",
@@ -14,9 +12,6 @@ const firebaseConfig = {
   measurementId: "G-ZJLHEDSVSB"
 };
 
-// Inicializamos Firebase
 const app = initializeApp(firebaseConfig);
-
-// Exportamos la Base de Datos (db) y la Autenticación (auth) para usarlas en los otros archivos
 export const db = getFirestore(app);
 export const auth = getAuth(app);
